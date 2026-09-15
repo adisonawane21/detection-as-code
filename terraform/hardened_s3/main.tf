c7a4ca0377f25e9c26629729c9e8570396e74652
Aditya Sonawane <aditya.sonawane.005@gmail.com>
2026-09-15 13:44:28 +0530

    Add abort_incomplete_multipart_upload to satisfy CKV_AWS_300

--- terraform/hardened_s3/main.tf
+++ terraform/hardened_s3/main.tf
@@ -35,11 +35,17 @@ resource "aws_s3_bucket_logging" "example" {
 
 resource "aws_s3_bucket_lifecycle_configuration" "example" {
   bucket = aws_s3_bucket.example.id
+
   rule {
     id     = "expire-old-logs"
     status = "Enabled"
+
     expiration {
       days = 90
+    }
+
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
     }
   }
 }
